a783f74a21b80535d395b1c29eb6cc05dc078509
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-02-27 22:13:28 +0000

    add main class

---
+++ main.py
@@ -0,0 +1,54 @@
+"""
+Main class
+
+--
+
+Author : DrLarck
+
+Last update : 27/02/20 (DrLarck)
+"""
+
+# dependancies
+import discord
+import asyncio
+import os
+
+class Main():
+    """
+    Run the bot and manages the bot configuration
+
+    - Method
+
+    `start()` : Run the bot
+    """
+
+    prefix = ["**"]
+    token = os.environ["pantheist-token"]
+
+    def start(self):
+        """
+        Run the bot
+
+        --
+
+        Return : `None`
+        """    
+
+        # init
+        client = None
+        activity = None
+
+        # defining the activity
+        activity = discord.Game("**help | v2 - BETA")
+
+        # defining the client
+        client = discord.ext.commands.Bot(self.prefix, activity = activity, help_command = None)
+
+        # run the bot
+        client.run(self.token)
+
+        return
+
+# run the Main class
+if(__name__ is "__main__"):
+    Main().start()
