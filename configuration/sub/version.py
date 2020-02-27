1012a0415de845bab5daac3aa70e8a5db5fbe032
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-02-27 23:44:57 +0000

    add Version() class

---
+++ configuration/sub/version.py
@@ -0,0 +1,23 @@
+"""
+Bot version class
+
+--
+
+Author : DrLarck
+
+Last update : 28/02/20 (DrLarck)
+"""
+
+class Version():
+    """
+    Represents the Bot version
+
+    - Attribute
+
+    `version` (`str`)
+
+    `phase` (`str`)
+    """
+
+    version = "2.0.0"
+    phase = "BETA"
